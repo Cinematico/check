3e20cd7b0600fcc1455bab7445849e144ed59846
Jason Schuller <hi@jason.sc>
2014-02-25 14:35:11 -0800

    Updating check.php for Cinematico

--- check.php
+++ check.php
@@ -1,12 +1,5 @@
 <?php
-/*******************************************************************
-*                                                                  *
-* If you see this text, it means that your server is not running   *
-* PHP. You can’t run Statamic.                                    *
-*                                                                  *
-* Ask your system administrator to install PHP.                    *
-*                                                                  *
-*******************************************************************/
+/* If you see this text, it means that your server is not running PHP, which means that you can’t run Cinematico. Ask your system administrator to install PHP. */
 ?>
 <?php
   $is_ready = true;
@@ -30,145 +23,203 @@
 ?>
 <!doctype html>
 <html>
-  <head>
-    <title>Statamic Server Check</title>
-    <link href='http://fonts.googleapis.com/css?family=Neuton:200,300|Crimson+Text:400,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="http://themes.statamic.com/london-wild/_themes/london-wild/css/london-wild.css" />
-  </head>
-  <body>
-    <div class="container" style="margin-top:100px;">
-      <div class="row">
-        <div class="span8 offset2">
-          <h1 id="logo">Statamic Server Check</h1>
-
-          <hr/ >
-
-          <?php if ($is_ready): ?>
-            <h3 class="ready">Good job, son. Your server seems worthy. You are ready for Statamic.</h3>
-          <?php else: ?>
-            <?php if ($have_rewrite): ?>
-              <h3 class="not-ready">Easy slugger, it seems that your server may not be ready for Statamic.</h3>
-              <p>It would appear your server may NOT be ready to run Statamic (0.9)</p>
+    <head>
+        <title>Cinematico Server Check</title>
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
+        <style>
+            body {
+                background: #ffffff;
+                font-family: 'Lato', sans-serif;
+                font-size: 14px;
+                line-height: 20px;
+                color: #404040;
+                text-rendering: optimizeLegibility;
+                -webkit-font-smoothing: antialiased;
+                -webkit-text-size-adjust: 100%;
+                width: auto;
+                height: auto;
+                margin: 0;
+                padding: 0;
+            }
+            
+            #container {
+                width: 520px;
+                padding: 150px;
+            }
+            
+            hr {
+                border: none;
+                background-color: #ebebeb;
+                height: 1px;
+                margin: 40px 0;
+            }
+            
+            h2 {
+                font-size: 34px;
+                color: #202020;
+                margin: 0 0 20px 0;
+                padding: 0;
+            }
+            
+            h3 {
+                font-size: 16px;
+                color: #202020;
+                margin: 0 0 10px 0;
+                padding: 0;
+            }
+            
+            p {
+                
+                padding: 0;
+            }
+            
+            ul {
+                list-style: none;
+                margin: 0 0 40px 0;
+                padding: 0;
+            }
+            
+            .pass {
+                color: #60b86b;
+            }
+            
+            .button {
+                background-color: #60b86b;
+                font-size: 14px;
+                line-height: 20px;
+                color: #ffffff;
+                text-decoration: none;
+                margin: 0 0 5px 0;
+                padding: 8px 14px 10px;
+                display: inline-block;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <h2>Cinematico Server Check</h2>
+            <?php if ($is_ready): ?>
+            <p>Congrats! Your server seems worthy. You are ready for Cinematico.</p>
+            <a class="button" href="http://cinemati.co">Get Cinematico</a>
             <?php else: ?>
-              <h3 class="not-ready">Curious, it seems that your server may not be ready.</h3>
-              <p>We could not determine if Mod Rewrite was enabled or not</p>
+            <?php if ($have_rewrite): ?>
+            <p>It seems that your server may not be ready for Cinematico.</p>
+            <?php else: ?>
+            <p>Curious, it seems that your server may not be ready. We could not determine if Mod Rewrite was enabled or not.</p>
             <?php endif ?>
-          <?php endif; ?>
+            <?php endif; ?>
+            
+            <hr>
+            
+            <h3>PHP Version</h3>
+            <?php if (version_compare(PHP_VERSION, '5.3', '>=')): ?>
+            <ul>
+                <li class="pass">Pass: <?php echo PHP_VERSION ?></li>
+            </ul>
+            <?php else: $failed = TRUE ?>
+            <ul>
+                <li class="fail">Failed: <?php echo PHP_VERSION ?></li>
+                <li class="note">Cinematico requires PHP 5.3 or newer.</li>
+            </ul>
+            <?php endif ?>
+            
+            <hr>
+            
+            <h3>PCRE UTF-8</h3>
+            <?php if ( ! function_exists('preg_match')): $failed = TRUE ?>
+            <ul>
+                <li class="fail">Failed</li>
+                <li class="fail">PCRE support is missing.</li>
+                <li class="note">Ask your system administrator to add <a href="http://php.net/pcre" target="_blank">PCRE</a> support to your server.</li>
+            </ul>
+            <?php elseif ( ! @preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
+            <ul>
+                <li class="fail">Failed</li>
+                <li class="fail">No UTF-8 support</li>
+                <li class="note"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with UTF-8 support.</li>
+            </ul>
+            <?php elseif ( ! @preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
+            <ul>
+                <li class="fail">Failed</li>
+                <li class="fail">No Unicode support</li>
+                <li class="note"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with Unicode property support.</li>
+            </ul>
+            <?php else: ?>
+            <ul>
+                <li class="pass">Pass</li>
+            </ul>
+            <?php endif ?>  
+            
+            <hr>          
 
-          <p>Here's what we looked at.</p>
-          <br>
+            <h3>Mod Rewrite</h3>
+            <?php $have_module = apache_is_module_loaded('mod_rewrite'); ?>
+            <?php if ($have_module): ?>
+            <ul>
+                <li class="pass">Pass</li>
+            </ul>
+            <?php elseif ($have_module == null): ?>
+            <ul>
+                <li class="unknown">Unknown</li>
+                <li class="note">We could not determine if Mod Rewrite was enabled or not.</li>
+            </ul>
+            <?php else: ?>
+            <ul>
+                <li class="fail">Failed</li>
+                <li class="note">Ask your system administrator to enable Mod Rewrite for your site.</li>
+            </ul>
+            <?php endif ?>
+            
+            <hr>
 
-          <table cellspacing="0">
-            <tr>
-              <th class="label">PHP Version</th>
-              <?php if (version_compare(PHP_VERSION, '5.3', '>=')): ?>
-                <td class="sign pass">Pass</td>
-                <td class="version"><?php echo PHP_VERSION ?></td>
-                <td class="instructions"></td>
-              <?php else: $failed = TRUE ?>
-                <td class="sign fail">Failed</td>
-                <td class="version"><?php echo PHP_VERSION ?></td>
-                <td class="instructions exclam">Statamic requires PHP 5.3 or newer</td>
-              <?php endif ?>
-            </tr>
+            <h3>cURL</h3>
+            <?php $have_curl = function_exists('curl_version') ? true : false; ?>
+            <?php if ($have_curl): ?>
+            <ul>
+                <li class="pass">Pass</li>
+            </ul>
+            <?php else: ?>
+            <ul>
+                <li class="fail">Failed</li>
+                <li class="note">Ask your system administrator to enable cURL for your site</li>
+            </ul>
+            <?php endif ?>
+            
+            <hr>
 
-            <tr>
-              <th class="label">PCRE UTF-8</th>
-              <?php if ( ! function_exists('preg_match')): $failed = TRUE ?>
-                <td class="sign fail">Failed</td>
-                <td class="version red">PCRE support is missing</td>
-                <td class="instructions exclam">Ask your system administrator to add <a href="http://php.net/pcre" target="_blank">PCRE</a> support to your server</td>
-              <?php elseif ( ! @preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
-                <td class="sign fail">Failed</td>
-                <td class="version red">No UTF-8 support</td>
-                <td class="instructions exclam"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with UTF-8 suppor.</td>
-              <?php elseif ( ! @preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
-                <td class="sign fail">Failed</td>
-                <td class="version red">No Unicode support</td>
-                <td class="instructions exclam"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with Unicode property support</td>
-              <?php else: ?>
-                <td class="sign pass">Pass</td>
-                <td class="version"></td>
-                <td class="instructions"></td>
-              <?php endif ?>
-            </tr>
-
-            <tr>
-              <th class="label">Mod Rewrite</th>
-              <?php $have_module = apache_is_module_loaded('mod_rewrite'); ?>
-              <?php if ($have_module): ?>
-                <td class="sign pass">Pass</td>
-                <td class="version"></td>
-                <td class="instructions"></td>
-              <?php elseif ($have_module == null): ?>
-                <td class="sign gloups">Unknown</td>
-                <td class="version">Unknown</td>
-                <td class="instructions exclam">We could not determine if Mod Rewrite was enabled or not</td>
-              <?php else: ?>
-                <td class="sign fail">Failed</td>
-                <td class="version red">Not enabled</td>
-                <td class="instructions exclam">Ask your system administrator to enable Mod Rewrite for your site</td>
-              <?php endif ?>
-            </tr>
-
-            <tr>
-              <th class="label">cURL</th>
-              <?php $have_curl = function_exists('curl_version') ? true : false; ?>
-              <?php if ($have_curl): ?>
-                <td class="sign pass">Pass</td>
-                <td class="version"></td>
-                <td class="instructions"></td>
-              <?php else: ?>
-                <td class="sign fail">Failed</td>
-                <td class="version red">Not enabled</td>
-                <td class="instructions exclam">Ask your system administrator to enable cURL for your site</td>
-              <?php endif ?>
-            </tr>
-
-            <tr>
-              <th class="label">GD Library</th>
-              <?php $gd = gdversion(); ?>
-              <?php if ($gd): ?>
-                <td class="sign pass">Pass</td>
-                <td class="version"></td>
-                <td class="instructions"></td>
-              <?php elseif ($gd == null): ?>
-                <td class="sign gloups">Unknown</td>
-                <td class="version">Unknown</td>
-                <td class="instructions exclam">We could not determine if GD Library was enabled or not</td>
-              <?php else: ?>
-                <td class="sign fail">Missing</td>
-                <td class="version red">Not enabled</td>
-                <td class="instructions exclam">You will still be able to use Statmic, you just won't be able to use any image manipulation features.</td>
-              <?php endif ?>
-            </tr>
-
-          </table>
-
-        <div id="footer">
-          <p class="faint" style="text-align:center">Powered by <a href="http://statamic.com">Statamic</a></p>
+            <h3>GD Library</h3>
+            <?php $gd = gdversion(); ?>
+            <?php if ($gd): ?>
+            <ul>
+                <li class="pass">Pass</li>
+            </ul>
+            <?php elseif ($gd == null): ?>
+            <ul>
+                <li class="unknown">Unknown</li>
+                <li class="note">We could not determine if GD Library was enabled or not</li>
+            </ul>
+            <?php else: ?>
+            <ul>
+                <li class="fail">Missing</li>
+                <li class="note">You will still be able to use Cinematico, you just won't be able to use any image manipulation features.</li>
+            </ul>
+            <?php endif ?>
         </div>
-      </div>
-    </div>
-    </div>
-
-  </body>
+    </body>
 </html>
 <?php
-
-  function gdversion() {
-    $gd = gd_info();
-    $ver = $gd['GD Version'];
-    return $ver;
-  }
-
-  function apache_is_module_loaded($mod_name) {
-    if (function_exists('apache_get_modules')) {
-      $modules = apache_get_modules();
-      return in_array($mod_name, $modules);
-    } else {
-      return null;
+    function gdversion() {
+        $gd = gd_info();
+        $ver = $gd['GD Version'];
+        return $ver;
     }
-  }
-
+    
+    function apache_is_module_loaded($mod_name) {
+        if (function_exists('apache_get_modules')) {
+            $modules = apache_get_modules();
+            return in_array($mod_name, $modules);
+        } else {
+            return null;
+        }
+    }
 ?>
